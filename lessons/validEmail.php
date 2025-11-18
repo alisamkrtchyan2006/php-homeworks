a35273cfe516adb8a9239d6a8bebc2c7d95455bc
alisamkrtchyan2006 <alisamkrtchyann@gmail.com>
2025-11-19 00:34:03 +0400

    valid email using string methods

--- lessons/validEmail.php
+++ lessons/validEmail.php
@@ -1,13 +1,41 @@
 <?php
 
-function isValidEmail(string $email): bool {
-    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
-}
+declare(strict_types=1);
 
+// function isValidEmail(string $email): bool {
+//     return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+// }
+
+
+// $email1 = "test@example.com";
+// $email2 = "invalid-email";
+
+// var_dump (isValidEmail($email1)); 
+// echo '<br><br>';
+// var_dump (isValidEmail($email2));
+
+
+
+function isValidEmail(string $email): bool {
+
+    if (strpos($email, '@') === false) return false;
+
+    [$local, $domain] = explode('@', $email, 2);
+
+    if (strlen($local) === 0) return false;
+
+    if (strpos($domain, '.') === false) return false;
+
+    [$host, $topLavelDomain] = explode('.', $domain, 2);
+
+    if (strlen($host) === 0 || strlen($topLavelDomain) < 2) return false;
+
+    return true;
+}
 
 $email1 = "test@example.com";
 $email2 = "invalid-email";
 
-var_dump (isValidEmail($email1)); 
+var_dump(isValidEmail($email1));
 echo '<br><br>';
-var_dump (isValidEmail($email2));
+var_dump(isValidEmail($email2));
